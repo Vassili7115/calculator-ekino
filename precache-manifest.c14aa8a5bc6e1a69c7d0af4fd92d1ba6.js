cb6a3de6d9a7130ab459a1f031e4cb1f6cef64e0
Matthieu <ranuccimatthieu@gmail.com>
2019-01-04 17:05:40 +0100

    Updates

---
+++ precache-manifest.c14aa8a5bc6e1a69c7d0af4fd92d1ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d767cdd42025d7d9ed8",
+    "url": "/calculator-ekino/static/css/main.fdcef835.chunk.css"
+  },
+  {
+    "revision": "0d767cdd42025d7d9ed8",
+    "url": "/calculator-ekino/static/js/main.0d767cdd.chunk.js"
+  },
+  {
+    "revision": "d2d9e3023efce29703b9",
+    "url": "/calculator-ekino/static/js/1.d2d9e302.chunk.js"
+  },
+  {
+    "revision": "4badd8cfcac54f5f55a8",
+    "url": "/calculator-ekino/static/js/runtime~main.4badd8cf.js"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/calculator-ekino/static/media/DS-DIGI.63f874d1.TTF"
+  },
+  {
+    "revision": "21be1206dc7c86c5d376f9bc62fee145",
+    "url": "/calculator-ekino/static/media/Graphik-Bold.21be1206.ttf"
+  },
+  {
+    "revision": "5a0182727e173a78322418be95ef9b02",
+    "url": "/calculator-ekino/index.html"
+  }
+];
